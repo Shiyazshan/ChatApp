2f28e2be3c301b49d246eabce16ade05748e332b
Shiyazshan <shiyazshan555@gmail.com>
2022-01-03 16:05:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { ChatEngine } from 'react-chat-engine'
+import ChatFeed from './components/ChatFeed';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine 
+       height= "100vh"
+       projectID="45d5c73d-aff7-453e-b746-d2dfaa9ec587"
+       userName="Shiyas"
+       userSecret="123123"
+       renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps} /> }
+    />
   );
 }
 
