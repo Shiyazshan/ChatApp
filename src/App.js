0766005757e1e9f9d7f29fbce50a2a4c5caf8ce8
Shiyazshan <shiyazshan555@gmail.com>
2022-01-03 16:51:37 +0530

    login credentials

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
-import { ChatEngine } from 'react-chat-engine'
+import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
-function App() {
+const projectID = '45d5c73d-aff7-453e-b746-d2dfaa9ec587';
+
+
+const App = ()=> {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine 
        height= "100vh"
-       projectID="45d5c73d-aff7-453e-b746-d2dfaa9ec587"
-       userName="Shiyas"
-       userSecret="123123"
+       projectID={projectID}
+       userName={localStorage.getItem('username')}
+       userSecret={localStorage.getItem('password')}
        renderChatFeed={(chatAppProps) => <ChatFeed { ...chatAppProps} /> }
+       onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
 }
